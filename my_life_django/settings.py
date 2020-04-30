2e623c75a3000a7ed559efb99d1a16411a9aee7d
Yoda <mihael.peric@hotmail.com>
2020-04-30 10:45:31 +0200

    settings

--- my_life_django/settings.py
+++ my_life_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'nhf4ibn7m6%j!iw6+lmm!l#(rsn1_$=gyjh10^elge520(gr3q'
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'slice-of-life-yodabikarbona.herokuapp.com']
 
 
 # Application definition
