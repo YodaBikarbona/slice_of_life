13dd3e88e78430fbf22a20c8266512e93dfd5227
Yoda <mihael.peric@hotmail.com>
2020-04-30 10:19:10 +0200

    settings

--- my_life_django/settings.py
+++ my_life_django/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nhf4ibn7m6%j!iw6+lmm!l#(rsn1_$=gyjh10^elge520(gr3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
